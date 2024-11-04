37e02d984990a61bd9dd124ef3289cfc21096f85
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-11-04 16:55:45 +0200

    changed routing to give home page

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterComponent from './components/RegisterComponent';
 import LoginComponent from './components/LoginComponent';
 import PaymentComponent from './components/PaymentComponent';
 import PaymentVerificationComponent from './components/PaymentVerificationComponent';
 
+function HomeComponent() {
+  return (
+    <div>
+      <h1>Welcome to the Payment Portal</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<HomeComponent />} />
         <Route path="/register" element={<RegisterComponent />} />
         <Route path="/login" element={<LoginComponent />} />
         <Route path="/make-payment" element={<PaymentComponent />} />
